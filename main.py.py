263413c3e4d9bbb278ff76cde912e80a30d79840
Hudson <hudsonrogeriojunior@hotmail.com>
2020-12-17 18:36:41 -0300

    First commit

---
+++ main.py.py
@@ -0,0 +1,15 @@
+
+
+
+def imprimir_tabuleiro(jogador,posiçao):
+	
+	print(" ____ ____ ____")
+	print("|    |    |    |")
+	print("|____|____|____|")
+	print("|    |    |    |")
+	print("|____|____|____|")
+	print("|    |    |    |")
+	print("|____|____|____|")
+
+
+	imprimit_tabuleiro(0,0)
